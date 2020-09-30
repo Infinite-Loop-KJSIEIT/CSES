fd61f3ac5929001cc76f1fe3ccf2b96675cf8c2d
vedantkokate07 <43782747+vedantkokate07@users.noreply.github.com>
2020-10-01 02:03:01 +0530

    organinig files

---
+++ Dynamic Programming/Dice Combinations/Dice Combinations.py
@@ -0,0 +1,13 @@
+n=int(input())
+mod=10**9+7
+dp=[0]*(n+1)
+dp[0]=1
+for i in range(1,n+1):
+    for x in range(1,7):
+        if x>i:
+            break
+        dp[i]+=dp[i-x]
+        dp[i]%=mod
+#print(dp)
+print(dp[n]%mod)
+    
