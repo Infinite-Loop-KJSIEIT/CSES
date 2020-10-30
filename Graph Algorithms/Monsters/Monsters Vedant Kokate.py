472bad3cf68c9b67dc3050f464555d7eb37a981f
vedantkokate07 <43782747+vedantkokate07@users.noreply.github.com>
2020-10-30 05:45:30 +0530

    coreections 2 plus adding more solutions

--- Graph Algorithms/Monsters/Monsters Vedant Kokate.py
+++ Graph Algorithms/Monsters/Monsters Vedant Kokate.py
@@ -1,13 +1,12 @@
 import time
 import math
-import sys 
+import sys
+from collections import deque
 def get_ints(): return map(int, sys.stdin.readline().strip().split())
 def ipi(): return int(sys.stdin.readline().strip())
 def ips(): return(sys.stdin.readline().strip())
 sys.setrecursionlimit(10**6)
     #Initialize a queue
- 
- 
  
 n,m=get_ints()
 a=[]
@@ -16,68 +15,50 @@ for i in range(n):
     l=[]
     l[:0]=s    
     a.append(l)
- 
-step=[["" for i in range(m)]for j in range(n)]
-step2=[["" for i in range(m)]for j in range(n)]
-vis=[[True for i in range(m)]for j in range(n)]
-sx=sy=0
-q=[]
+q=deque()
+step=[[0 for i in range(m)]for j in range(n)]
 for i in range(n):
     for j in range(m):
         if a[i][j]=='A':
             sx=i
             sy=j
         if a[i][j]=='M':
-            q.append([i,j])
-            vis[i][j]=False
-r=[0,0,1,-1]
-c=[1,-1,0,0]
-d=['R','L','D','U']
- 
+            a[i][j]='#'
+            q.append((i,j))
+D=((0,1,'R'),(0,-1,'L'),(1,0,'D'),(-1,0,'U'))
+R={'L':(0,1),'R':(0,-1),'D':(-1,0),'U':(1,0)}
 while q:
-   # print(q)
-    s=q.pop(0)
-    x1=s[0]
-    y1=s[1]
-    for i in range(4):
-        x=x1+r[i]
-        y=y1+c[i]
-        #print(x,y)
-        if x<n and x>-1 and y<m and y>-1 and a[x][y]!='#' and vis[x][y]:
-            vis[x][y]=False
-            q.append([x,y])
-            step[x][y]=str(step[x1][y1])+d[i]
-q.append([sx,sy])
-flag=False
+    x,y=q.popleft()
+    for r,c,d in D:
+        X,Y=x+r,y+c
+        if 0<=X<n and 0<=Y<m and a[X][Y]!='#' and step[X][Y]==0:
+            step[X][Y]=step[x][y]+1
+            q.append((X,Y))
+
+q.append((sx,sy))
+step[sx][sy]=0
+a[sx][sy]='#'
+
 while q:
-    #print(q)
-    s=q.pop(0)
-    x1=s[0]
-    y1=s[1]
-    if x1==0 or y1==0 or x1==n-1 or y1==m-1:
-        print("YES")
-        print(len(step2[x1][y1]))
-        print(step2[x1][y1])
-        flag=True
-        break
-    
-    for i in range(4):
+    x,y=q.popleft()
+    if x==0 or x==n-1 or y==0 or y==m-1:
+        ans=[]
+        while x!=sx or y!=sy:            
+            ans.append(a[x][y])
+            x1,y1=R[a[x][y]]
+            x+=x1
+            y+=y1
         
-        x=x1+r[i]
-        y=y1+c[i]
-       # print(x,y)
-        if x<n and x>-1 and y<m and y>-1 and a[x][y]=='.' :
-            step2[x][y]=step2[x1][y1]+d[i]
-            a[x][y]='#'
-            if len(step2[x][y])>=len(step[x][y]) and len(step[x][y])!=0:
-                continue
-            q.append([x,y])
-if not flag:
-    print("NO")
+        print('YES')
+        print(len(ans))
+        print(''.join(reversed(ans)))
+        raise SystemExit
+    for r,c,d in D:
+        X,Y=x+r,y+c
+        if 0<=X<n and 0<=Y<m and a[X][Y]!='#' and (step[X][Y]==0 or step[X][Y]>step[x][y]+1):
+            step[X][Y]=step[x][y]+1
+            a[X][Y]=d
+            q.append((X,Y))
+print("NO")
  
-'''for i in step2:
-    print(i)
- 
-for i in step:
-    print(i)'''
-                
+
